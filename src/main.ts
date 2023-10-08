d768e09f7c961f2d618ae36780e24b0b0aa4a14e
Wyatt H <jhawes@ucsc.edu>
2023-10-08 15:48:53 -0700

    Step 1 complete

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,15 @@ import "./style.css";
 const app: HTMLDivElement = document.querySelector("#app")!;
 
 const gameName = "Wyatt's game";
+const buttonName = "🍍";
 
 document.title = gameName;
 
 const header = document.createElement("h1");
 header.innerHTML = gameName;
+
+const button = document.createElement("button");
+button.textContent = buttonName;
+
 app.append(header);
+app.append(button);
