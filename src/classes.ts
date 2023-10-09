6160c8b53292dfe8dc82458205649ffe9773bf70
Wyatt H <jhawes@ucsc.edu>
2023-10-08 20:16:38 -0700

    Step 5 Complete

---
+++ src/classes.ts
@@ -0,0 +1,88 @@
+export class world_state {
+  per_click_increase: number;
+  pineapples_per_second: number;
+  total_pineapples: number;
+  elements_to_add: HTMLElement[] = [];
+  total_pineapple_count_element: HTMLDivElement;
+  upgrade_buttons: upgrade_button[] = [];
+  pineapple_per_second_count_element: HTMLDivElement | null = null;
+
+  constructor(total_pineapple_counter_element: HTMLDivElement) {
+    this.elements_to_add = [];
+    this.upgrade_buttons = [];
+
+    this.per_click_increase = 1;
+    this.pineapples_per_second = 0;
+    this.total_pineapples = 0;
+    this.total_pineapple_count_element = total_pineapple_counter_element;
+  }
+
+  update_counter(count: number, text: string = " Pineapples Chopped!") {
+    this.total_pineapples = this.total_pineapples + count;
+    this.total_pineapple_count_element.innerHTML = this.total_pineapples + text;
+    this.check_buttons();
+  }
+
+  check_buttons() {
+    this.upgrade_buttons.forEach((btn) => {
+      if (this.total_pineapples >= btn.cost) {
+        btn.button_element.disabled = false;
+      } else {
+        btn.button_element.disabled = true;
+      }
+    });
+  }
+}
+
+// eslint-disable-next-line no-unused-vars
+export class upgrade_button {
+  button_element: HTMLButtonElement;
+  cost: number;
+  per_second_increase: number;
+  price_scaling: number;
+  emoji_name: string;
+  world_state: world_state;
+
+  constructor(
+    emoji: string,
+    cost: number,
+    psi: number,
+    p_scaling: number,
+    world: world_state,
+  ) {
+    this.emoji_name = emoji;
+    this.button_element = document.createElement("button");
+    this.button_element.style.fontSize = "20pt";
+    this.cost = cost;
+    this.per_second_increase = psi;
+    this.price_scaling = p_scaling;
+    this.world_state = world;
+
+    this.button_element.innerHTML =
+      this.emoji_name +
+      " Cost: " +
+      cost +
+      " | " +
+      this.per_second_increase +
+      " pps";
+    this.button_element.disabled = true;
+    world.elements_to_add.push(this.button_element);
+
+    this.button_element.addEventListener("click", () => {
+      world.update_counter(cost * -1, undefined);
+      this.cost = this.cost * this.price_scaling;
+      this.update_pineapple_per_second();
+    });
+    this.world_state.upgrade_buttons.push(this);
+  }
+
+  update_pineapple_per_second() {
+    this.world_state.pineapples_per_second =
+      this.world_state.pineapples_per_second + this.per_second_increase;
+
+    if (this.world_state.pineapple_per_second_count_element != null) {
+      this.world_state.pineapple_per_second_count_element.innerHTML =
+        this.world_state.pineapples_per_second + " / pps";
+    }
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,79 +1,86 @@
 import "./style.css";
+import { world_state, upgrade_button } from "./classes";
+("./classes.ts");
 
-const per_click_increase: number = 1;
-const pineapples_per_second: number = 2;
-let total_pineapples: number = 0;
+//Upgrade Ideas 🥢 📏 🥄 ✂️ 🪒 🔧 🗡️ 🪓 ⛏ 🔪
 
 const app: HTMLDivElement = document.querySelector("#app")!;
 
-//Added elements array
-let elements_to_add: HTMLElement[] = [];
-elements_to_add = [];
+// Create counter to attach to the world state
+const total_pineapple_counter_element = document.createElement("div");
+total_pineapple_counter_element.style.fontSize = "40pt";
+total_pineapple_counter_element.innerHTML = 0 + " Pineapples Chopped!";
+
+const player: world_state = new world_state(total_pineapple_counter_element);
+player.elements_to_add.push(total_pineapple_counter_element);
 
 //Title & Header Element
 const gameName = "Wyatt's game";
 const buttonName = "🍍";
 document.title = gameName;
-create_new_element("h1", "", gameName);
-
-// Counter Element
-const total_pineapple_counter_element = create_new_element(
-  "div",
-  "40px",
-  total_pineapples + " Pineapples Chopped!",
-);
+create_new_element("h1", "", gameName, true);
 
 // Button Element
 const button = create_new_element("button", "50px", buttonName);
 
 // Clicker Element
 button.addEventListener("click", () => {
-  update_counter(
-    total_pineapple_counter_element,
-    per_click_increase,
-    " Pineapples Chopped!",
-  );
+  player.update_counter(player.per_click_increase);
 });
 
 //Auto Click Element
-create_new_element("div", "30px", pineapples_per_second + " / pps");
-start_auto_counter(total_pineapple_counter_element, pineapples_per_second);
+const autoclick_element = create_new_element(
+  "div",
+  "30px",
+  player.pineapples_per_second + " / pps",
+);
+player.pineapple_per_second_count_element = autoclick_element as HTMLDivElement;
+start_auto_counter(player);
 
+//First upgrade button
+new upgrade_button("🥢", 10, 1, 1, player);
+
+//-----------------
+//----Functions----
+//-----------------
 //Add all elements in order
-elements_to_add.forEach((elem) => {
+player.elements_to_add.forEach((elem) => {
   app.append(elem);
 });
 
 // Create a new HTML element function
-function create_new_element(type: string, font_size: string, text: string) {
+function create_new_element(
+  type: string,
+  font_size: string,
+  text: string,
+  unshift: boolean = false,
+): HTMLElement | HTMLButtonElement | HTMLDivElement {
   const new_element = document.createElement(type);
   new_element.style.fontSize = font_size;
   new_element.innerHTML = text;
-  elements_to_add.push(new_element);
+
+  if (unshift) {
+    player.elements_to_add.unshift(new_element);
+  } else {
+    player.elements_to_add.push(new_element);
+  }
 
   return new_element;
 }
 
 //Update counter function
-function update_counter(
-  count_element: HTMLDivElement | HTMLElement,
-  count: number,
-  text: string = " Pineapples Chopped!",
-) {
-  total_pineapples = total_pineapples + count;
-  count_element.innerHTML = total_pineapples + text;
-}
 
 //Automatically update counter based on pineapples_per_second
-function start_auto_counter(elem: HTMLElement, pps: number) {
+function start_auto_counter(player_world_state: world_state) {
   let past_date = performance.now();
   window.requestAnimationFrame(check_for_counter_update);
 
   function check_for_counter_update() {
-    const milliseconds_per_update = 1000 / pps;
+    const milliseconds_per_update =
+      1000 / player_world_state.pineapples_per_second;
 
     if (performance.now() - past_date > milliseconds_per_update) {
-      update_counter(elem, 1);
+      player.update_counter(1);
       past_date = performance.now();
     }
     window.requestAnimationFrame(check_for_counter_update);
